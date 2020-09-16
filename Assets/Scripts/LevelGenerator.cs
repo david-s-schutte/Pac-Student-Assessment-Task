2249da0802e1b4665bde93731583633f4afa4b5e
david-s-schutte <david.schutte01@gmail.com>
2020-09-16 13:42:48 +1000

    Began work on LevelGenerator script
    Began working on the level generator script by cretaing prefabs out of objects that need to be spawned

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    private int[,] levelMapQuad1;
+    public GameObject background;
+    public GameObject empty;
+    public GameObject oCorner;
+    public GameObject oWall;
+    public GameObject iCorner;
+    public GameObject iWall;
+    public GameObject tJunct;
+    public GameObject pellet;
+    public GameObject powerPellet;
+    public GameObject bonusCherry;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 13; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
