f4ad07f9586b172354346f8e2741c06f5d66f5bb
david-s-schutte <david.schutte01@gmail.com>
2020-09-17 15:45:43 +1000

    Level Generated
    The Level is now able to generate. Still need to determine the rotation of certain pieces. Also updated certain sprites to better suit the generator

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,14 +4,10 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    private int[,] levelMapQuad1;
-    private int[,] levelMapQuad2;
-    private int[,] levelMapQuad3;
-    private int[,] levelMapQuad4;
 
-    int columns, rows;
+    int columns;
+    int rows;
 
-    public GameObject background, empty;
     public GameObject oCorner;
     public GameObject oWall;
     public GameObject iCorner;
@@ -21,56 +17,109 @@ public class LevelGenerator : MonoBehaviour
     public GameObject powerPellet;
     public GameObject bonusCherry;
 
+    int[,] levelMap = {
+            {1,2,2,2,2,2,2,2,2,2,2,2,2,7,7,2,2,2,2,2,2,2,2,2,2,2,2,1},
+            {2,5,5,5,5,5,5,5,5,5,5,5,5,4,4,5,5,5,5,5,5,5,5,5,5,5,5,2},
+            {2,5,3,4,4,3,5,3,4,4,4,3,5,4,4,5,3,4,4,4,3,5,3,4,4,3,5,2},
+            {2,6,4,0,0,4,5,4,0,0,0,4,5,4,4,5,4,0,0,0,4,5,4,0,0,4,6,2},
+            {2,5,3,4,4,3,5,3,4,4,4,3,5,3,3,5,3,4,4,4,3,5,3,4,4,3,5,2},
+            {2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2},
+            {2,5,3,4,4,3,5,3,3,5,3,4,4,4,4,4,4,3,5,3,3,5,3,4,4,3,5,2},
+            {2,5,3,4,4,3,5,4,4,5,3,4,4,3,3,4,4,3,5,4,4,5,3,4,4,3,5,2},
+            {2,5,5,5,5,5,5,4,4,5,5,5,5,4,4,5,5,5,5,4,4,5,5,5,5,5,5,2},
+            {1,2,2,2,2,1,5,4,3,4,4,3,0,4,4,0,3,4,4,3,4,5,1,2,2,2,2,1},
+            {0,0,0,0,0,2,5,4,3,4,4,3,0,3,3,0,3,4,4,3,4,5,2,0,0,0,0,0},
+            {0,0,0,0,0,2,5,4,4,0,0,0,0,0,0,0,0,0,0,4,4,5,2,0,0,0,0,0},
+            {0,0,0,0,0,2,5,4,4,0,3,4,4,0,0,4,4,3,0,4,4,5,2,0,0,0,0,0},
+            {2,2,2,2,2,1,5,3,3,0,4,0,0,0,0,0,0,4,0,3,3,5,1,2,2,2,2,2},
+            {0,0,0,0,0,0,5,0,0,0,4,0,0,0,0,0,0,4,0,0,0,5,0,0,0,0,0,0},
+            {2,2,2,2,2,1,5,3,3,0,4,0,0,0,0,0,0,4,0,3,3,5,1,2,2,2,2,2},
+            {0,0,0,0,0,2,5,4,4,0,3,4,4,0,0,4,4,3,0,4,4,5,2,0,0,0,0,0},
+            {0,0,0,0,0,2,5,4,4,0,0,0,0,0,0,0,0,0,0,4,4,5,2,0,0,0,0,0},
+            {0,0,0,0,0,2,5,4,3,4,4,3,0,3,3,0,3,4,4,3,4,5,2,0,0,0,0,0},
+            {1,2,2,2,2,1,5,4,3,4,4,3,0,4,4,0,3,4,4,3,4,5,1,2,2,2,2,1},
+            {2,5,5,5,5,5,5,4,4,5,5,5,5,4,4,5,5,5,5,4,4,5,5,5,5,5,5,2},
+            {2,5,3,4,4,3,5,4,4,5,3,4,4,3,3,4,4,3,5,4,4,5,3,4,4,3,5,2},
+            {2,5,3,4,4,3,5,3,3,5,3,4,4,4,4,4,4,3,5,3,3,5,3,4,4,3,5,2},
+            {2,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,2},
+            {2,5,3,4,4,3,5,3,4,4,4,3,5,3,3,5,3,4,4,4,3,5,3,4,4,3,5,2},
+            {2,6,4,0,0,4,5,4,0,0,0,4,5,4,4,5,4,0,0,0,4,5,4,0,0,4,6,2},
+            {2,5,3,4,4,3,5,3,4,4,4,3,5,4,4,5,3,4,4,4,3,5,3,4,4,3,5,2},
+            {2,5,5,5,5,5,5,5,5,5,5,5,5,4,4,5,5,5,5,5,5,5,5,5,5,5,5,2},
+            {1,2,2,2,2,2,2,2,2,2,2,2,2,7,7,2,2,2,2,2,2,2,2,2,2,2,2,1},
+        };
+
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(background);
-
-        rows = 30;
-        columns = 28;
-
-        Quaternion cornerRotation = Quaternion.identity;
-
-        levelMapQuad1 = new int[columns, rows];
-        levelMapQuad2 = new int[columns, rows];
-        levelMapQuad3 = new int[columns, rows];
-        levelMapQuad4 = new int[columns, rows];
+        rows = 28;
+        columns = 29;
 
         for (int c = 0; c < columns; c++)
         {
             for (int r = 0; r < rows; r++)
             {
-                levelMapQuad1[c, r] = Random.Range(0, 8);
-                SpawnTile(c, r, levelMapQuad1[c, r]);
+                SpawnObject(c, r, levelMap[c, r]);
                 
             }
         }
-        
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        //for (int c = 0; c < columns; c++)
+        //{
+        //    for (int r = 0; r < rows; r++)
+        //    {
+        //        determineRotation(c, r, levelMap[c, r]);
+
+        //    }
+        //}
+
     }
 
-    private void SpawnTile(int x, int y, int value)
+    private void SpawnObject(int x, int y, int value)
     {
-        Vector3 spawnPosition = new Vector3(x - (columns - 0.5f), y - (rows - 0.5f), 0f);
+        Vector3 spawnPosition = new Vector3(-(rows - y), (columns - x));
+        Quaternion rotation = Quaternion.identity;
+        rotation.eulerAngles = determineRotation(x, y, value);
+
         switch (value)
         {
-            //case 0: Instantiate(empty, spawnPosition, Quaternion.identity); break;
-            case 1: Instantiate(oCorner, spawnPosition, Quaternion.identity); break;
-            case 2: Instantiate(oWall, spawnPosition, Quaternion.identity); break;
-            case 3: Instantiate(iCorner, spawnPosition, Quaternion.identity); break;
-            case 4: Instantiate(iWall, spawnPosition, Quaternion.identity); break;
-            case 5: Instantiate(pellet, spawnPosition, Quaternion.identity); break;
-            case 6: Instantiate(powerPellet, spawnPosition, Quaternion.identity); break;
-            case 7: Instantiate(tJunct, spawnPosition, Quaternion.identity); break;
+            case 1: Instantiate(oCorner, spawnPosition, rotation); break;
+            case 2: Instantiate(oWall, spawnPosition, rotation); break;
+            case 3: Instantiate(iCorner, spawnPosition, rotation); break;
+            case 4: Instantiate(iWall, spawnPosition, rotation); break;
+            case 5: Instantiate(pellet, spawnPosition, rotation); break;
+            case 6: Instantiate(powerPellet, spawnPosition, rotation); break;
+            case 7: Instantiate(tJunct, spawnPosition, rotation); break;
         }
     }
 
-    Vector3 determineRotation()
+    Vector3 determineRotation(int x, int y, int value)
     {
-        return new Vector3(0f, 0f, 90f);
+        if (value == 1) //oCorner
+        {
+            
+        }
+
+        if (value == 2) //oWall
+        {
+            
+        }
+
+        if (value == 3) //iCorner
+        {
+            
+        }
+
+        if (value == 4) //iWall
+        {
+
+        }
+
+        if (value == 5) //tJunct
+        {
+
+        }
+
+        return new Vector3(0f, 0f, 0f);
     }
 }
