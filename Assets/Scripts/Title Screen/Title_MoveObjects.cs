bf4101e38653270706636dae7ee566ee86238725
david-s-schutte <david.schutte01@gmail.com>
2020-09-30 01:09:40 +1000

    50 Percent Part 1
    Finished:
    - Created StartScene
    - Created Title and Subtitle
    - Created Border
    - Plays jingle on loop
    - Placed PacStudent and Ghosts in title screen and partially created script
    
    To Do:
    - Finish script for gameobjects
    - Create level buttona
    - Display high score

---
+++ Assets/Scripts/Title Screen/Title_MoveObjects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is used to move the pacstudent and ghost sprites on the title screen
+
+public class Title_MoveObjects : MonoBehaviour
+{
+    private Vector3 spawnpos; 
+    private Tweener tweener;
+
+    public Vector3 endpos = new Vector3(10f, -2f, 0f);
+    private float duration = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnpos = transform.position;
+        tweener = GetComponent<Tweener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x <= endpos.x)
+        {
+            transform.Translate(duration *Time.deltaTime, 0f, 0f);
+        }
+        else if (transform.position.x >= endpos.x)
+        {
+            transform.position = spawnpos;
+        }
+    }
+}
